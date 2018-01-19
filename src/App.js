b8451e7572e2e5b93d31cd2421f9faa6cbbc8b4f
Watcharinz <tewze123@hotmail.com>
2018-01-19 16:30:53 +0700

    Start

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+import _ from 'lodash'
+ 
+ const students =[
+   {id:'3', name: 'Tew', score: 10},
+   {id:'9', name: 'Karn', score: 30},
+   {id:'27', name: 'Non', score: 90}
+ ]
+ 
+ const StudentsLine = (props)=>(
+   <div> {props.id} {props.name} = {props.score} </div>
+ )
+  
+  class App extends Component {
+    render() {
+      return (
+       <div >
+         {
+           _.map(students, s => <StudentsLine {...s} key={s.id}/>)
+         }
+        </div>
+      );
+    }
   }
-}
-
-export default App;
+ 
+  export default App;
