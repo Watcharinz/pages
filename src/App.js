af5e8e233360e92eda63413fbc911677237e327c
Watcharinz <tewze123@hotmail.com>
2018-01-19 16:33:44 +0700

    Start2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
-import _ from 'lodash'
+ import _ from 'lodash'
+ import {
+   BrowserRouter as Router,
+   Route,
+   Link
+ } from 'react-router-dom'
  
  const students =[
-   {id:'3', name: 'Tew', score: 10},
-   {id:'9', name: 'Karn', score: 30},
-   {id:'27', name: 'Non', score: 90}
+   {id:'3', name: 'Tew', score: 100},
+   {id:'9', name: 'Karn', score: 70},
+   {id:'27', name: 'Non', score: 60}
  ]
  
- const StudentsLine = (props)=>(
-   <div> {props.id} {props.name} = {props.score} </div>
- )
-  
-  class App extends Component {
-    render() {
-      return (
-       <div >
-         {
-           _.map(students, s => <StudentsLine {...s} key={s.id}/>)
-         }
-        </div>
-      );
-    }
-  }
+ const Home = () => (<div>Home</div>)
  
-  export default App;
+ const Students = () => (
+   <div>
+      {
+        _.map(students, s => <StudentLink {...s} key={s.id}/>)
+      }
+      <Route path="/students/:id" component={StudentContainer}/>
+    </div>
+  )
+  
+ const StudentContainer = ({match}) => {
+   let s = _.find(students, ['id', match.params.id])
+   return (
+     <StudentLine {...s} key={s.id}/>
+   )
+ }
+ 
+  const StudentLink = ({id, name}) => (
+    <div><Link to={`/students/${id}`}>{name}</Link></div>
+  )
+ const StudentLine = ({id, name, score}) => (
+     <div>{id} {name} = {score}</div>
+   )
+ 
+ class App extends Component {
+   render() {
+     return (
+       <Router>
+       <div >
+         
+           <Route exact path="/" component={Home}/>
+           <Route path="/students" component={Students}/> 
+         
+       </div>
+       </Router>
+     );      
+   }
+ }
+ 
+ export default App;
